53b2a98abe857675a83637a7a9a8853b90a14b37
timjini <hatim.jini@gmail.com>
2022-03-18 16:02:06 +0300

    css update

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,7 @@
-// Entry point for the build script in your package.json
+// // Entry point for the build script in your package.json
+const btn = document.querySelector("button.mobile-menu-button");
+const menu = document.querySelector(".mobile-menu");
+btn.addEventListener("click", () => {
+	menu.classList.toggle("hidden");
+    console.log(menu)
+});
