bbe0387719e7971aa0ceec207916deca632c847b
timjini <hatim.jini@gmail.com>
2023-01-06 00:22:07 +0300

    main

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+     <h1 className='text-blue-600 p-5'>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
